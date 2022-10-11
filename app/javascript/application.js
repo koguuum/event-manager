6df8590464c897c981434af49112298e0610f08d
koguuum <koguum43wk@gmail.com>
2022-10-12 01:18:12 +0900

    print hello world

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,13 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import HelloMessage from './components/App';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+document.addEventListener('DOMContentLoaded', () => {
+  root.render(<HelloMessage name="World" />);
+});
