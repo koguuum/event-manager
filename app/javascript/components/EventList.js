447eb6ee4dc643bb49752ccfff5a5f80c3bcb8fe
koguuum <koguum43wk@gmail.com>
2022-10-16 10:34:18 +0900

    fetch event list

---
+++ app/javascript/components/EventList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const EventList = ({ events }) => {
+  const renderEvents = (eventArray) => {
+    eventArray.sort((a, b) => new Date(b.event_date) - new Date(a.event_date));
+
+    return eventArray.map((event) => (
+      <li key={event.id}>
+        {event.event_date}
+        {' - '}
+        {event.event_type}
+      </li>
+    ));
+  };
+
+  return (
+    <section>
+      <h2>Events</h2>
+      <ul>{renderEvents(events)}</ul>
+    </section>
+  );
+};
+
+export default EventList;
