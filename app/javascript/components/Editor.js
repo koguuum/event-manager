0ed8c6a29da060fc5d17da93bcf2d1486bddee95
koguuum <koguum43wk@gmail.com>
2022-10-16 09:57:39 +0900

    fetch data

--- app/javascript/components/Editor.js
+++ app/javascript/components/Editor.js
@@ -7,6 +7,24 @@ const Editor = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await window.fetch('/api/events');
+        if (!response.ok) throw Error(response.statusText);
+        const data = await response.json();
+        setEvents(data);
+      } catch (error) {
+        setIsError(true);
+        console.error(error);
+      }
+
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <>
       <Header />
